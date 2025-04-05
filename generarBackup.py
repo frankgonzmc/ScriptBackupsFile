fb84dff3548c52ce7c52be452ee97fa844ca2f17
Frank Puertas <frankgzmc@gmail.com>
2025-04-05 12:57:40 -0500

    Version-Filtrar el ingreso de rutas validas.

--- generarBackup.py
+++ generarBackup.py
@@ -1,11 +1,17 @@
+from pathlib import Path
+
 def menu():
     print("Ingrese la direccion de la carpeta que desea respaldar:")
     link = input("Direccion: ")
-    
+    link_path = Path(link)
 
-
+    #Comprobar si la carpeta existe
+    if link_path.exists():
+        print("La carpeta existe")
+    else:
+        print("La carpeta no existe")
 
 
 
 if __name__ == "__main__":
-    pass
+    menu()
